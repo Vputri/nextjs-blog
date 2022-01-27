6e37154e427f023b1906b9b29d62b01bc206536b
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2022-01-27 10:19:06 +0700

    modif query

--- components/query.js
+++ components/query.js
@@ -1,10 +1,20 @@
-import React from "react";  
 import { useQuery } from "@apollo/react-hooks";
 
 const Query = ({ children, query }) => {  
   const { data, loading, error, fetchMore } = useQuery(query);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Artikel tidak tersedia</p>;
+  
+  if (loading) return (
+  	<div className="mx-3 px-5">
+  		<p className="mx-4 px-5">Loading</p>
+  	</div>
+  );
+
+  if (error) return (
+  	<div className="mx-3 px-5">
+  		<p className="mx-4 px-5">Artikel tidak tersedia</p>
+  	</div>
+  )
+
   return children({ data, fetchMore });
 };
 
